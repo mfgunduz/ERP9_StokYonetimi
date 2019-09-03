6ba88a947fe734d135081d6c0f39bfd1ac7f5b07
Furkan Gündüz <gunduzfurkan20@gmail.com>
2019-09-03 10:53:00 +0300

    .

---
+++ Sy.Core/Abstracts/AuditBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class AuditBase // nesne olarak kullanmak istiyoruz ama instence oluşmasını istemiyoruz
+    {
+        
+        [StringLength(50)]
+        public string CreatedUser { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [StringLength(50)]
+        public string UpdatedUser { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
---
+++ Sy.Core/Abstracts/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sy.Core.Abstracts
+{
+    public abstract class BaseEntity<TKey> : IEntity<TKey>
+    {
+        [Key]
+
+        public TKey Id { get; set; }
+    }
+}
